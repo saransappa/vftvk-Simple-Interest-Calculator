43ea638c0363e6d20a6de35dee568df1fb07da9a
Saran Sappa <saran.sappa@gmail.com>
2021-07-05 03:18:44 +0000

    updated

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    document.getElementById("result").innerHTML = "If you deposit "+principal+ ",\nat an interest rate of "+rate+"%.\n You will receive an amount of "+interest+",\n in the year "+year;
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
+function checkPrincipal(){
+    var principal = document.getElementById("principal").value;
+    if(principal<=0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+	else{
+		compute();
+	}
+}
