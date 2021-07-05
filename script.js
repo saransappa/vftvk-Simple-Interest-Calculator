1f0920d0fea6cf67a147f741cb15afd2694e1bbf
Saran Sappa <saran.sappa@gmail.com>
2021-07-05 03:30:34 +0000

    updated script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML = "If you deposit "+principal+ ",<br/>at an interest rate of "+rate+"%.<br/> You will receive an amount of "+interest+",<br/> in the year "+year;
+    document.getElementById("result").innerHTML = "If you deposit <mark>"+principal+ "</mark>,<br/>at an interest rate of <mark>"+rate+"%</mark>.<br/> You will receive an amount of <mark>"+interest+"</mark>,<br/> in the year <mark>"+year+"</mark>";
 }
 
 function updateRate() 
